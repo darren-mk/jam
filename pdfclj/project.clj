f584f80e973b2e7483ac0b1f9e9578e73f07b01f
Darren Minsoo Kim <darren.minsoo.kim@gmail.com>
2022-06-27 07:36:57 -0400

    test pdf encryption in clojure

---
+++ pdfclj/project.clj
@@ -0,0 +1,10 @@
+(defproject pdfclj "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [clj-pdf "2.6.1"]
+                 [crypto-password "0.3.0"]
+                 [org.apache.pdfbox/pdfbox "3.0.0-alpha2"]]
+  :repl-options {:init-ns pdfclj.core})
