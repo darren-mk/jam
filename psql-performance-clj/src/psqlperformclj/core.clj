e0de154bc01a451031302b9833217358e3499544
Darren Kim <darren.minsoo.kim@gmail.com>
2023-03-20 16:46:36 -0400

    tested postgres performance

---
+++ psql-performance-clj/src/psqlperformclj/core.clj
@@ -0,0 +1,29 @@
+(ns psqlperformclj.core
+  (:require
+   [clojure.string :as str]
+   [next.jdbc :as jdbc]))
+
+(def db {:dbtype "postgres" :dbname "helenium"})
+
+(def ds (jdbc/get-datasource db))
+
+(jdbc/execute!
+ ds [(str "create table address ("
+          "id serial primary key, "
+          "name text, "
+          "email text)")])
+
+(time
+ (jdbc/execute!
+  ds [(str "insert into address(name, email) values "
+           (str/join ", " (mapv (fn [i] (str "('name-" i "', 'email-" i "')"))
+                                (range 10000))))]))
+;; "Elapsed time: 92.019375 msecs"
+
+(time
+ (jdbc/execute! ds ["select * from address where name = 'name-8888';"]))
+;; "Elapsed time: 22.84925 msecs"
+;; "Elapsed time: 21.541875 msecs"
+;; "Elapsed time: 18.861334 msecs"
+;; "Elapsed time: 15.300458 msecs"
+;; "Elapsed time: 26.232042 msecs"
