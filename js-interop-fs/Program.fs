da5532defef7dd31f779435b4640a946754fda6b
Darren Kim <darren.minsoo.kim@gmail.com>
2025-04-04 18:19:57 -0400

    learn how fable interop with js

---
+++ js-interop-fs/Program.fs
@@ -0,0 +1,19 @@
+﻿open Fable.Core
+open Browser
+
+[<Import("hello", "./greet.js")>]
+let hello : string -> unit = jsNative
+
+hello "yo!"
+
+[<Global>]
+let console: JS.Console = jsNative
+console.log "hey, i am console, as you know"
+
+[<Global("console")>]
+let logger: JS.Console = jsNative
+logger.log "hey, i am named logger."
+
+let div = document.createElement "div"
+div.innerHTML <- "Yay, Darren!!"
+document.body.appendChild div |> ignore
