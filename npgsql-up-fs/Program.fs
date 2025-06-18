5a9c116ba953fbf731bb1adb552d841e8c22229d
Darren Kim <darren.minsoo.kim@gmail.com>
2025-06-18 06:50:25 -0400

    test npgsql with postgres in fs

---
+++ npgsql-up-fs/Program.fs
@@ -0,0 +1,35 @@
+﻿open System
+open Npgsql
+open Npgsql.FSharp
+open System.Data
+
+type Person = {
+    Id : Guid
+    Username : string
+    Email : string }
+
+let connectionString: string =
+    "Host=localhost;Port=5432;Username=localuser;Password=localpass;Database=dapper_up_fs_db"
+
+let conn : IDbConnection =
+    new NpgsqlConnection(connectionString)
+
+let newPerson: Person =
+    { Id = Guid.NewGuid()
+    ; Username = "Roman"
+    ; Email = "Provaznik" }
+
+let getAllPersons (connectionString: string) : Person list =
+    connectionString
+    |> Sql.connect
+    |> Sql.query "SELECT * FROM people" // FS and LIB CANNOT DETECT THIS
+    |> Sql.execute (fun read ->
+        {
+            Id = read.uuid "id"
+            Username = read.text "name" // FS and LIB CANNOT DETECT THIS
+            Email = read.text "email?" // FS and LIB CANNOT DETECT THIS
+        })
+
+Console.WriteLine (getAllPersons)
+
+printfn "Hello from F#"
