83f5ffe12c193f777201a9eae7d76255616603c0
Darren Kim <darren.minsoo.kim@gmail.com>
2025-09-24 00:02:33 -0400

    try quartz properties config

--- quartz-up-clj/src/quartz_up_clj/core.clj
+++ quartz-up-clj/src/quartz_up_clj/core.clj
@@ -1,21 +1,57 @@
 (ns quartz-up-clj.core
+  (:gen-class)
   (:require
-   [clojurewerkz.quartzite.scheduler :as qs]
-   [clojurewerkz.quartzite.triggers :as qt]
-   [clojurewerkz.quartzite.jobs :as qj]
-   [clojurewerkz.quartzite.schedule.cron :as qsc]))
+   [clojure.java.io :as io])
+  (:import
+   (java.util Properties)
+   (org.quartz.impl StdSchedulerFactory)
+   (org.quartz JobBuilder TriggerBuilder
+               SimpleScheduleBuilder)))
 
-(qj/defjob NoOpJob [_ctx]
-  (println "yay!"))
+(def props (Properties.))
+
+(defn load-props []
+  (let [stream (io/input-stream
+                (io/resource
+                 "quartz.properties"))]
+    (.load props stream)))
+
+(def scheduler (atom nil))
+
+(defn start-scheduler []
+  (let [factory (StdSchedulerFactory.)]
+    (reset! scheduler (.getScheduler factory))
+    (doto @scheduler (.start))))
+
+(defn stop-scheduler []
+  (when @scheduler
+    (.shutdown @scheduler)
+    (println "scheduler stopped")))
+
+(defrecord SimpleJob []
+  org.quartz.Job
+  (execute [_this _context]
+    (println "Hello from SimpleJob!")))
+
+(defn schedule-simple-job []
+  (let [job (-> (JobBuilder/newJob SimpleJob)
+                (.withIdentity "simple-job" "group1")
+                (.build))
+        trigger (-> (TriggerBuilder/newTrigger)
+                    (.withIdentity "simple-trigger" "group1")
+                    (.startNow)
+                    (.withSchedule (-> (SimpleScheduleBuilder/simpleSchedule)
+                                       (.withIntervalInSeconds 5)
+                                       (.repeatForever)))
+                    (.build))]
+    (.scheduleJob @scheduler job trigger)))
 
 (comment
-  (let [s (qs/start (qs/initialize))
-        job (qj/build
-              (qj/of-type NoOpJob)
-              (qj/with-identity (qj/key "jobs.noop.1")))
-        trigger (qt/build
-                  (qt/with-identity (qt/key "triggers.1"))
-                  (qt/start-now)
-                  (qt/with-schedule (qsc/schedule
-                                     (qsc/cron-schedule "0/2 * * * * ?"))))]
-    (qs/schedule s job trigger)))
+  (load-props)
+  (stop-scheduler)
+  (start-scheduler)
+  (schedule-simple-job)
+  (println "Scheduler Name:"
+           (.getSchedulerName
+            (.getMetaData @scheduler))))
+
